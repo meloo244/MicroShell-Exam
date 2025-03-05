ca46219ea696df3d37f068db6c68d0526b0b4cd7
Mel <melvynoo143@gmail.com>
2025-03-05 16:08:06 +0100

    microshell

--- microshell_full.c
+++ microshell_full.c
@@ -10,35 +10,35 @@ int	err(char *str)
 	return (1);
 }
 
-int	cd(char **av, int i)
+int	cd(char **argv, int i)
 {
 	if (i != 2)
 		return (err("error: cd: bad arguments\n"));
-	if (chdir(av[1]) == -1)
-		return (err("error: cd: cannot change directory to "), err(av[1]), err("\n"));
+	if (chdir(argv[1]) == -1)
+		return (err("error: cd: cannot change directory to "), err(argv[1]), err("\n"));
 	return (0);
 }
 
-int	exec(char **av, int i, char **env)
+int	exec(char **argv, int i, char **envp)
 {
 	int	fd[2];
 	int	status;
-	int	has_pipe = av[i] && !strcmp(av[i], "|");
+	int	has_pipe = argv[i] && !strcmp(argv[i], "|");
 
-	if (!has_pipe && !strcmp(*av, "cd"))
-		return (cd(av, i));
+	if (!has_pipe && !strcmp(*argv, "cd"))
+		return (cd(argv, i));
 	if (has_pipe && pipe(fd) == -1)
 		return (err("error: fatal\n"));
 	int	pid = fork();
 	if (!pid)
 	{
-		av[i] = 0;
+		argv[i] = 0;
 		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return (err("error: fatal\n"));
-		if (!strcmp(*av, "cd"))
-			return (cd(av, i));
-		execve(*av, av, env);
-		return (err("error: cannot execute "), err(*av), err("\n"));
+		if (!strcmp(*argv, "cd"))
+			return (cd(argv, i));
+		execve(*argv, argv, envp);
+		return (err("error: cannot execute "), err(*argv), err("\n"));
 	}
 	waitpid(pid, &status, 0);
 	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
@@ -46,21 +46,21 @@ int	exec(char **av, int i, char **env)
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
-int	main(int ac, char **av, char **env)
+int	main(int argc, char **argv, char **envp)
 {
 	int	i = 0;
 	int status = 0;
 
-	if (ac > 1)
+	if (argc > 1)
 	{
-		while (av[i] && av[++i])
+		while (argv[i] && argv[++i])
 		{
-			av += i;
+			argv += i;
 			i = 0;
-			while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
 				i++;
 			if (i)
-				status = exec(av, i, env);
+				status = exec(argv, i, envp);
 		}
 	}
 	return (status);
